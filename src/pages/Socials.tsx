1347a68726bbde96e5ab97ccf4e57fd6872f29f4
KairenVk <kamilsurdacki01@gmail.com>
2024-03-03 22:17:53 +0100

    First iteration of 'socials' page

--- src/pages/Socials.tsx
+++ src/pages/Socials.tsx
@@ -1,4 +1,30 @@
 function Socials() {
-    return <h1>Socials</h1>
-};
+    return (
+        <>
+            <button name='Furaffinity' className='socialButton'>
+                <img className='icon' src='Fa_logo.svg' width='32' height='32' alt='Furaffinity icon'/>
+                <a className='navButton' rel="noreferrer" target='_blank'
+                   href='https://www.furaffinity.net/user/kairenvk'>Furaffinity</a>
+            </button>
+            <button name='Telegram' className='socialButton'>
+                <img className='icon' src='telegram.svg' width='32' height='32' alt='Telegram icon'/>
+                <a className='navButton' rel="noreferrer" target='_blank'
+                   href='https://t.me/KairenVk'>Telegram</a>
+            </button>
+            <div id='Discord' className='socialButton'>
+                <img className='icon' src='discord.svg' width='32' height='32' alt='Discord icon'/>
+                <button className='navButton tooltip' onClick={copy_discord_username}>Discord</button>
+            </div>
+            <button name='Steam' className='socialButton'>
+                <img className='icon' src='steam.svg' width='32' height='32' alt='Steam icon'/>
+                <a className='navButton' rel="noreferrer" target='_blank'
+                   href='https://steamcommunity.com/id/KairenVk/'>Steam</a>
+            </button>
+        </>
+    )
+}
+
+function copy_discord_username() {
+    navigator.clipboard.writeText('kairen_vanspark')
+}
 export default Socials;
